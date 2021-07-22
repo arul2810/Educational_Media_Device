f4cc1cefec3d3ade9b9850a93a490808ed460c0b
arul2810 <arul2810@gmail.com>
2021-07-23 02:44:09 +0530

    Add files via upload

---
+++ MiniProject/Images/image.h
@@ -0,0 +1,167 @@
+/* 
+
+	File Name : image.h
+	
+	File Description: Contains the array with RGB565 information for images to be plotted to LT24 Display. 
+	
+	Author: Arul Prakash Samathuvamani for ELEC5620M 
+	
+	Changelog: 
+	
+*/
+
+
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+// Contains array for animation images
+
+// ----------------------------------------------------------------------
+
+extern const unsigned short anime1 [57600]; // Intro Animation image 1
+
+extern const unsigned short anime2 [57600]; // Intro Animation image 2
+
+// Following contains array for image for count 1
+
+//-------------------------------------------------------------------------
+
+extern const unsigned short one [57600]; // One Count
+
+//--------------------------------------------------------------------------
+
+// Following arrays contains images for cout2 
+
+extern const unsigned short two [57600]; // Two image one
+
+extern const unsigned short two_two [57600]; // Two image two
+
+//---------------------------------------------------------------------------
+// Followin array contains images for count 3
+
+extern const unsigned short three [57600]; 
+
+extern const unsigned short three_two [57600];
+
+extern const unsigned short three_three [57600];
+
+//-----------------------------------------------------------------------------
+// Following contains array information for image count4
+
+extern const unsigned short four [57600];
+
+extern const unsigned short four_two [57600];
+
+extern const unsigned short four_three [57600];
+
+extern const unsigned short four_four [57600];
+
+//------------------------------------------------------------------------------
+// Following contains array information for image count5
+
+extern const unsigned short five[57600];
+
+extern const unsigned short five_two[57600];
+
+extern const unsigned short five_three [57600];
+
+extern const unsigned short five_four [57600];
+
+extern const unsigned short five_five [57600];
+
+// -------------------------------------------------------------------------------
+// Following contains array information for image count6
+
+extern const unsigned short six [57600];
+
+extern const unsigned short six_two [57600];
+
+extern const unsigned short six_three [57600];
+
+extern const unsigned short six_four [57600];
+
+extern const unsigned short six_five [57600];
+
+extern const unsigned short six_six [57600];
+
+// ---------------------------------------------------------------------------------
+// Following contains array information for image count seven
+
+extern const unsigned short seven [57600];
+
+extern const unsigned short seven_two [57600];
+
+extern const unsigned short seven_three [57600];
+
+extern const unsigned short seven_four [57600];
+
+extern const unsigned short seven_five [57600];
+
+extern const unsigned short seven_six [57600];
+
+extern const unsigned short seven_seven [57600];
+
+// ----------------------------------------------------------------------------------
+// Following contains array information for image count eight
+
+extern const unsigned short eight [57600];
+
+extern const unsigned short eight_two [57600];
+
+extern const unsigned short eight_three [57600];
+
+extern const unsigned short eight_four [57600];
+
+extern const unsigned short eight_five [57600];
+
+extern const unsigned short eight_six [57600];
+
+extern const unsigned short eight_seven [57600];
+
+extern const unsigned short eight_eight [57600];
+
+// -------------------------------------------------------------------------------------
+// Following contains array information for image count nine
+
+extern const unsigned short nine [57600];
+
+extern const unsigned short nine_two [57600];
+
+extern const unsigned short nine_three [57600];
+
+extern const unsigned short nine_four [57600];
+
+extern const unsigned short nine_five [57600];
+
+extern const unsigned short nine_six [57600];
+
+extern const unsigned short nine_seven [57600];
+
+extern const unsigned short nine_eight [57600];
+
+extern const unsigned short nine_nine [57600];
+
+// -------------------------------------------------------------------------------------
+// Following contains array information for image count ten
+
+extern const unsigned short ten [57600];
+
+extern const unsigned short ten_two [57600];
+
+extern const unsigned short ten_three [57600];
+
+extern const unsigned short ten_four [57600];
+
+extern const unsigned short ten_five [57600];
+
+extern const unsigned short ten_six [57600];
+
+extern const unsigned short ten_seven [57600];
+
+extern const unsigned short ten_eight [57600];
+
+extern const unsigned short ten_nine [57600];
+
+extern const unsigned short ten_ten [57600];
+
+#endif /* IMAGE_H_ */
